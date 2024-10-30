9bb3f925b966df40002a5c53d6309322039952e8
Eduardo Miranda <eduardo.dpm@gmail.com>
2024-10-30 11:54:04 -0300

    Hello World no Streamlit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AutomeetAI
 Criando uma ferramenta de automação de atas de reunião com tecnologia de IA com Python
+
+
+# Hello World no Streamlit
+Para rodar esta aplicação de forma local execute o seguinte comando no terminal:
+```sh
+streamlit run hello_world_streamlit.py
+```
+Isso abrirá um navegador web com a sua aplicação "Hello World App"
---
+++ hello_world_streamlit.py
@@ -0,0 +1,10 @@
+# Importamos o módulo streamlit com o alias 'st'
+# Dessa forma, podemos usar 'st' para acessar as funcionalidades do Streamlit
+import streamlit as st
+
+# Usamos a função title() do Streamlit para colocar um título na nossa aplicação web
+st.title('Hello World App')
+
+# Usamos a função write() para exibir um texto simples na página
+# Esta função é bastante versátil e pode também mostrar dados e exibir gráficos
+st.write('Hello World!')
